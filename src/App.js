fb9c32cd0e056b7d18abe2cf808ae607be82a5c2
AlphaH7 <Alistier.hea@gmail.com>
2023-10-10 23:42:08 +0100

    DEV: added bootstrap and sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
+import './scss/app.scss';
+import "bootstrap/scss/bootstrap.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+  return ( 
+    <div className="App container">
+      <header className="App-header col-md-12">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const SamplePage = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6">
+                    <h1>Left Side</h1>
+                </div>
+                <div className="col-md-6">
+                    <h1>Right Side</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SamplePage;
